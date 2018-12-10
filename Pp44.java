c6fc5834b3e6634a19726eac7b4bac0ec87c7f89
Pd3072 <45767002+Pd3072@users.noreply.github.com>
2018-12-10 11:21:28 -0600

    Add files via upload

---
+++ Pp44.java
@@ -0,0 +1,49 @@
+/**
+ * @(#)Pp44.java
+ *
+ *
+ * @author 
+ * @version 1.00 2018/12/10
+ */
+
+
+public class Pp44 {
+
+    public static void main (String[] args) 
+    	{
+    		
+    		final int ROLLS = 1000;
+    		int boxCars = 0, num1, num2;
+    		
+    		Die die1 = new Die();
+    		Die die2 = new Die();
+    		
+    		for (int roll = 1; roll <= ROLLS; roll++)
+    		{
+    			num1 = die1.roll();
+    			num2 = die2.roll();
+    			
+    		if (num1 == 6 && num2 == 6)
+    			boxCars++;	
+    		}
+    		
+    		
+    		System.out.println ("Number of rolls: " + ROLLS);
+    		System.out.println ("Number of box cars: " + boxCars);
+    		System.out.println ("Ratio: " + (double)boxCars/ROLLS);
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    	}
+    
+    
+}
